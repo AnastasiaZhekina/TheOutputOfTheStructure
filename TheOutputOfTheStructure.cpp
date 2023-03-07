2abeb0dbcbb9bf98a292e59d62f3f1ba554e1a28
AnastasiaZhekina <yfcnzjkzdbnz09@gmail.com>
2023-03-07 22:48:45 +0500

    frst

---
+++ TheOutputOfTheStructure.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <string>
+#include <windows.h>
+using namespace std;
+
+struct address {
+    string city;
+    string street;
+    int house;
+    int apartment;
+    int index;
+};
+void foo(struct address adss);
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    address adss;
+    
+    cin >> adss.city >> adss.street >> adss.house >> adss.apartment >> adss.index;
+
+    foo(adss);
+}
+
+void foo(struct address adss) {
+    cout << "Город: " << adss.city << endl <<
+        "Улица: " << adss.street << endl <<
+        "Номер дома: " << adss.house << endl <<
+        "Номер квартиры " << adss.apartment << endl <<
+        "Индекс " << adss.index << endl;
+};
